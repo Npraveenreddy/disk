94a919716900b2a57930b93fee232b1d276710b6
PraveenReddy I <narayana.praveen143@gmail.com>
2022-01-24 08:14:50 +0530

    Update disk.sh

--- disk.sh
+++ disk.sh
@@ -1,17 +1,41 @@
-#!/bin/bash
-THRESHOLD1=60
-THRESHOLD1=90
+echo -e "Select the Option From below:\n"
 
-EMAIL=narayana.praveen143@gmail.com
+# -e option in echo command is used to
+# enable interpretation of backslash escapes.
 
-#PART=yourdiskpart
+echo -e "\n
+[ 1 ] For Only the Disk-Name and Used-Space \n
+[ 2 ] For Only the Disk-Name and its Size \n
+[ 3 ] To print Disk-Name and File-System \n
+[ 4 ] To see all fields in DiskUsage \n"
 
-PART=/boot
-USE=`df -h |grep $PART | awk '{ print $5 }' | cut -d'%' -f1`
+# to take the user input
+read userInput
 
-if [ $USE -gt $THRESHOLD1 ]; then
-  echo "60% Percent Used: $USE" | mail -s "Disk Usage Rate " -r narayana.praveen143@gmail.com $EMAIL
-elif [ $USE -gt $THRESHOLD2 ]; then
-  echo "60% Percent Used: $USE" | mail -s "Disk Usage Rate " -r narayana.praveen143@gmail.com $EMAIL
-  
+# if to check the user input.
+if [ $userInput == 1 ];
+then
+
+# -h is used for producing human readable and
+# --output is used to specify field.
+df -h --output = source,used
+elif [ $userInput == 2 ];
+
+then
+df -h --output=source,size
+elif [ $userInput == 3 ];
+then
+
+# "source" argument is for listing name of the source directory,
+# "fstype" shows the file system type like ext4.
+df -h --output=source,fstype
+elif [ $userInput == 4 ];
+then
+
+# -a is used for all the fields.
+df -ha
+else
+
+# if any wrong input is given.
+echo "!!!!!!!!Wrong Output!!!!!!!!"
 fi
